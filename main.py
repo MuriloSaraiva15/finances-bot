11dbedfa9b4888eaf2336153817c09bfbcb1e62b
Murilo <muriloaguiar32@gmail.com>
2025-12-02 21:34:09 -0300

    base pronta

--- main.py
+++ main.py
@@ -4,24 +4,21 @@ import requests
 import json
 
 bot_token = "8278123611:AAH7P9WBQHoF5AsYFNNGaN6Xf1625JkBZHg"
-telegramUrl = "https://api.telegram.org/bot" + bot_token
-webAppUrl = "https://script.google.com/macros/s/AKfycbwv05rchOoFj1eHhQx93HUd_mxKplMrSheYIMaEWj2D1v394EuJBaYauARtSiCkJuhLow/exec"
+telegramUrl = f"https://api.telegram.org/bot{bot_token}"
+webAppUrl = "https://script.google.com/macros/s/AKfycbw7RWRfqEOqqadSAZJBPnWWkOIBHwDx883OyLb46u8u-fOR9Q1i_-UiKKQ6UT3Era1LZw/exec"
 
 
 # Criando webhook
+def set_webhook():
+    url =  f"{telegramUrl}/setWebhook?url={webAppUrl}"
+    response = requests.get(url)
+    return response.json()
 
-def setWebhookUrl():
-    webhookUrl = telegramUrl+"/setWebhook?url="+webAppUrl
-    print(webhookUrl)
-
-# Configurando start
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
-    await update.message.reply_text("Está funcionando?")
+if __name__ == "__main__":
+    resultado = set_webhook()
+    print(resultado)
 
 
-if __name__ == '__main__':
-    application = Application.builder().token(bot_token).build()
-    application.add_handler(CommandHandler("start", start))
-    application.run_polling()    
+
+
 
